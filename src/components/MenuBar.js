6690a9cfc56dcfa405f8fa0e3921235005edab7c
chris <chris.kuo216@gmail.com>
2021-12-02 01:52:13 -0500

    added menubar labels, set css layouts for menubar

---
+++ src/components/MenuBar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./MenuBar.css";
+
+function MenuBar() {
+  return (
+    <div id="navbar">
+      <ul>
+        <li>
+          <label>Kyudo</label>
+        </li>
+        <li>
+          <label>NZKF</label>
+        </li>
+        <li>
+          <label>Locations</label>
+        </li>
+        <li>
+          <label>Contact Us</label>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default MenuBar;
