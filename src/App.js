8358582c364d36b2ae5ff1dd053d1bd8230ccb48
Chris Kuo <chris.kuo216@gmail.com>
2021-12-14 03:08:45 -0500

    Revert "added docker support, adding basic carousel slide"
    This reverts commit 9b102e1f6611a15a6ea1938036e91721859917e0.
    
    Revert previous commit, create new branch for carousel

--- src/App.js
+++ src/App.js
@@ -1,39 +1,15 @@
 import React from "react";
 import MenuBar from "./components/MenuBar";
-import Carousel, { CarouselItem } from "./components/Carousel";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const carouselPhotos = [
-    {
-        id: 'p1',
-        title: 'photo1',
-        URL: 'https://www.ikyf.org/images/index_s/main06.jpg'
-    },
-    {
-        id: 'p2',
-        title: 'photo2',
-        URL: 'https://www.ikyf.org/images/index_s/main05.jpg'
-    },
-    {
-        id: 'p3',
-        title: 'photo3',
-        URL: 'https://www.ikyf.org/images/index_s/main07.jpg'
-    }
-    
-]
   return (
     <div className="App">
       <div id="navbar">
         {/* insert component here */}
         <MenuBar />
       </div>
-    <Carousel>
-      <CarouselItem><img src={carouselPhotos[0].URL} /></CarouselItem>
-      <CarouselItem><img src={carouselPhotos[1].URL} /></CarouselItem>
-      <CarouselItem><img src={carouselPhotos[2].URL} /></CarouselItem>
-    </Carousel>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
